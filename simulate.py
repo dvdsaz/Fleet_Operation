eeb7730cf7455212c505bdfc60e55a9a91f07be3
dvdsaz <dvdsaz18@gmail.com>
2026-08-17 21:18:06 -0700

    updated json

---
+++ simulate.py
@@ -0,0 +1,11 @@
+import json 
+with open("fleet.json", "r") as file:
+    fleet = json.load(file)
+
+
+for i in range(len(fleet)):
+    fleet[i]["speed"] = fleet[i]["speed"] - 1
+
+with open("fleet.json",'w') as file:
+    fleet = json.dump(fleet,file, indent =4)
+
