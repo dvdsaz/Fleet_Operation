528f373bf1dcf848b16014654332ec97478b2a84
dvdsaz <dvdsaz18@gmail.com>
2026-08-17 21:51:35 -0700

    Update every 5 seconds

--- simulate.py
+++ simulate.py
@@ -1,11 +1,11 @@
 import json 
-with open("fleet.json", "r") as file:
-    fleet = json.load(file)
+import time 
 
-
-for i in range(len(fleet)):
-    fleet[i]["speed"] = fleet[i]["speed"] - 1
-
-with open("fleet.json",'w') as file:
-    fleet = json.dump(fleet,file, indent =4)
-
+while True:
+    with open("fleet.json", "r") as file:
+        fleet = json.load(file)
+    for i in range(len(fleet)):
+        fleet[i]["speed"] = fleet[i]["speed"] - 1
+    with open("fleet.json",'w') as file:
+        fleet = json.dump(fleet,file, indent =4)
+    time.sleep(5)
