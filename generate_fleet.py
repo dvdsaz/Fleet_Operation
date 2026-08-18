95624bd58722c848d1dfba41c00ae900bb34d11d
dvdsaz <dvdsaz18@gmail.com>
2026-08-17 20:38:22 -0700

    Added speed, temperature,

--- generate_fleet.py
+++ generate_fleet.py
@@ -4,10 +4,16 @@ fleet = []
 for i in range(10):
     id = f'AV_{i}'
     battery = random.randint(30,80)
+    speed = random.randint(25,45)
+    temperature = random.randint(20,100)
     status = random.choice(['Active','Charging'])
-    fleet.append({"id": id,"battery":battery,"status":status})
+    fleet.append({"id": id,
+                  "spped": speed,
+                  "battery":battery,
+                  "temperature": temperature,
+                  "status":status})
 
 with open("fleet.json", "w") as file:
     json.dump(fleet,file, indent =4)
 
-    
+
