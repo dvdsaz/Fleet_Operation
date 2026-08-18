7aaa9e054b3d6759a89957e2ba2cd5a64690b996
dvdsaz <dvdsaz18@gmail.com>
2026-08-17 20:16:26 -0700

    Added Script to Generate Json

--- generate_fleet.py
+++ generate_fleet.py
@@ -1,1 +1,12 @@
+import random
+import json
+fleet = []
+for i in range(10):
+    id = f'AV_{i}'
+    battery = random.randint(30,80)
+    status = random.choice(['Active','Charging'])
+    fleet.append({"id": id,"battery":battery,"status":status})
+
+with open("fleet.json", "w") as file:
+    json.dump(fleet,file, indent =4)
 
