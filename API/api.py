ece8af545eb94d90b9b1c3ae0db65ed7e6ab123c
dvdsaz <dvdsaz18@gmail.com>
2026-08-21 13:58:45 -0700

    Corrected Fleet readind and added 2 api points

--- API/api.py
+++ API/api.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+fleet = {}
+with open("../Simulation/fleet.json", "r") as file:
+        fleet = json.load(file)
+
 @app.get("/Vehicles")
 def get_Vehicles():
-    return {"HEEEE"}
+    return fleet
+
+@app.get("/Vehicles/{vid}")
+def get_Vehicles(vid: str):
+    for i in fleet:
+         if i["id"] == vid:
+              return i
+    return {"404 error: Vehicle not Located"}
