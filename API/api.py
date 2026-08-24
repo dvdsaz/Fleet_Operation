598fc4625f56b83681544041ed7d22c4694f78f3
dvdsaz <dvdsaz18@gmail.com>
2026-08-24 14:52:12 -0700

    Added end points for createing, reading, updating, and deleting

--- API/api.py
+++ API/api.py
@@ -5,14 +5,43 @@ app = FastAPI()
 fleet = {}
 with open("../Simulation/fleet.json", "r") as file:
         fleet = json.load(file)
-
-@app.get("/Vehicles")
+        
+#Get all Vehicle in FLeet
+@app.get("/vehicles")
 def get_Vehicles():
     return fleet
 
-@app.get("/Vehicles/{vid}")
+#Get a specific Vehcile
+@app.get("/vehicles/{vid}")
 def get_Vehicles(vid: str):
     for i in fleet:
          if i["id"] == vid:
               return i
-    return {"404 error: Vehicle not Located"}
+    return {"404 error: Vehicle not Located"} 
+
+
+#Add a vehicle to the List
+@app.post("/vehicles")
+def post_vehicle(vid: str):
+    veh = {
+        "id": vid,
+        "speed": 0,
+        "battery": 80,
+        "temperature": 50,
+        "status": "Active"
+    }
+    fleet.append(veh) 
+    with open("../Simulation/fleet.json", "w") as file:
+            json.dump(fleet, file, indent=4)
+    return veh
+#Delete specific vehicle
+@app.delete("/vehicle")
+def get_Vehicles(vid: str):
+    for i in fleet:
+         if i["id"] == vid:
+              fleet.remove(i)
+              with open("../Simulation/fleet.json", "w") as file:
+                    json.dump(fleet, file, indent=4)
+              return {"Vehicle Found and Deleted"}
+
+    return {"404 error: Vehicle not Located"} 
